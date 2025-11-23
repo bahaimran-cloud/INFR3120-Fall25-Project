3221f84c2bd421a42ef5142b213a783c1c580b52
HR1789 <haider.raza2@ontariotechu.net>
2025-11-23 15:17:47 -0500

    Added the login.ejs and register.ejs code and routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const passport = require('passport');
+let DB = require('../config/db');
+let userModel = require('../models/user');
+let User = userModel.User;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Career Pointer' });
@@ -13,5 +18,105 @@ router.get('/aboutus', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Career Pointer - Contact Us' });
 });
+
+// Get method for login
+router.get('/login', function(req,res,next){
+  if(!req.user)
+  {
+    res.render('auth/login',
+      {
+      title:'Career Pointer - Login',
+      message: req.flash('loginMessage')
+      }
+
+    )
+  }
+  else
+  {
+    return res.redirect("/")
+  }
+});
+
+// Post method for login
+router.post('/login', function(req,res,next){
+  passport.authenticate('local',(err,user,info)=>{
+    if(err)
+    {
+      return next(err);
+    }
+    if(!user)
+    {
+      req.flash('loginMessage','AuthenticationError');
+      return res.redirect('/login');
+    }
+    req.login(user,(err)=>{
+    if(err)
+    {
+      return next(err);
+    }
+    return res.redirect("/applications");
+    })
+  })(req,res,next)
+});
+
+// Get method for register
+router.get('/register', function(req,res,next){
+  if(!req.user)
+  {
+    res.render('auth/register',
+      {
+      title:'Career Pointer - Register',
+      message: req.flash('registerMessage')
+      }
+
+    )
+  }
+  else
+  {
+    return res.redirect("/")
+  }
+});
+
+// Post method for register
+router.post('/register', function(req,res,next){
+  let newUser = new User({
+    username: req.body.username,
+    //password: req.body.password,
+    email:req.body.email,
+    displayName: req.body.displayName
+  })
+  User.register(newUser, req.body.password, (err)=>{
+    if(err)
+    {
+      console.log("Error:Inserting the new user");
+      if(err.name=="UserExistingError")
+      {
+        req.flash('registerMessage','Registration Error:User already Exist');
+      }
+      return res.render('auth/register',
+        {
+          title:'Career Pointer - Register',
+          message:req.flash('registerMessage')
+        }
+      )
+    }
+    else{
+      return passport.authenticate('local')(req,res,()=>{
+        res.redirect("/applications");
+      })
+    }
+  })
+});
+router.get('/logout',function(req,res,next){
+req.logout(function(err)
+{
+  if(err)
+  {
+    return next(err)
+  }
+})
+res.redirect("/");
+})
+
 
 module.exports = router;
